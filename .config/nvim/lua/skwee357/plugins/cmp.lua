4fb3ffb89ba79d0614f9c2aa07419978feb9eada
Dmitry Kudryavtsev <dmitry.kudryavtsev@forter.com>
2022-04-28 17:30:40 +0300

    added cmp navigation

--- .config/nvim/lua/skwee357/plugins/cmp.lua
+++ .config/nvim/lua/skwee357/plugins/cmp.lua
@@ -1,4 +1,5 @@
 local cmp = require'cmp'
+local luasnip = require'luasnip'
 
 local cmp_kinds = {
   Text = "  ",
@@ -28,6 +29,11 @@ local cmp_kinds = {
   TypeParameter = "  "
 }
 
+local has_words_before = function()
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+  return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
+end
+
 cmp.setup({
   view = {
     entries = "custom"
@@ -42,7 +48,7 @@ cmp.setup({
     -- REQUIRED - you must specify a snippet engine
     expand = function(args)
       -- vim.fn["vsnip#anonymous"](args.body) -- For `vsnip` users.
-      require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+      luasnip.lsp_expand(args.body) -- For `luasnip` users.
       -- require('snippy').expand_snippet(args.body) -- For `snippy` users.
       -- vim.fn["UltiSnips#Anon"](args.body) -- For `ultisnips` users.
     end,
@@ -57,6 +63,26 @@ cmp.setup({
      c = cmp.mapping.close(),
    }),
    ['<CR>'] = cmp.mapping.confirm({ select = false }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+   ['<Tab>'] = cmp.mapping(function(fallback)
+     if cmp.visible() then
+       cmp.select_next_item()
+     elseif luasnip.exapnd_or_jumpable() then
+       luasnip.expand_or_jump()
+     elseif has_words_before() then
+       cmp.complete()
+     else
+       fallback()
+     end
+   end, { 'i', 's' }),
+   ['<S-Tab>'] = cmp.mapping(function(fallback)
+     if cmp.visible() then
+       cmp.select_prev_item()
+     elseif luasnip.jumpable(-1) then
+       luasnip.jump(-1)
+     else
+       fallback()
+     end
+   end, { 'i', 's' }),
   },
   sources = cmp.config.sources({
     { name = 'nvim_lsp' },
