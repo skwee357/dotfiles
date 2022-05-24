d92d3ca638014057a82136395e3ad0980e3ad068
skwee357 <ko.dmitri@gmail.com>
2022-05-24 22:39:46 +0300

    wgsl

--- .config/nvim/lua/skwee357/plugins/treesitter.lua
+++ .config/nvim/lua/skwee357/plugins/treesitter.lua
@@ -1,3 +1,10 @@
+local parser_config = require 'nvim-treesitter.parsers'.get_parser_configs()
+parser_config.wgsl = {
+  install_info = {
+    url = "https://github.com/szebniok/tree-sitter-wgsl",
+    files = { "src/parser.c" }
+  }
+}
 require 'nvim-treesitter.configs'.setup {
   -- One of "all", "maintained" (parsers with maintainers), or a list of languages
   ensure_installed = {
@@ -34,7 +41,8 @@ require 'nvim-treesitter.configs'.setup {
     "toml",
     "typescript",
     "vim",
-    "yaml"
+    "yaml",
+    "wgsl"
   },
 
   -- Install languages synchronously (only applied to `ensure_installed`)
