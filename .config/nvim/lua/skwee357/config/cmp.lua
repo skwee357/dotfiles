a6a3d5ca673e4a9a57368916cad2ee8fa2eb7be3
skwee357 <ko.dmitri@gmail.com>
2022-12-12 23:16:36 +0200

    fix cmp

--- .config/nvim/lua/skwee357/config/cmp.lua
+++ .config/nvim/lua/skwee357/config/cmp.lua
@@ -3,7 +3,7 @@ local luasnip = require 'luasnip'
 local lspkind = require 'lspkind'
 
 local has_words_before = function()
-  local line, col = table.unpack(vim.api.nvim_win_get_cursor(0))
+  local line, col = unpack(vim.api.nvim_win_get_cursor(0))
   return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
@@ -45,8 +45,8 @@ cmp.setup({
     ['<Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
-      elseif luasnip.jumpable() then
-        luasnip.jump(1)
+      elseif luasnip.expand_or_jumpable() then
+        luasnip.expand_or_jump()
       elseif has_words_before() then
         cmp.complete()
       else
