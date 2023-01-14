0feb65c758eee417fa001bedfc1f5c6752f496e4
skwee357 <ko.dmitri@gmail.com>
2023-01-14 22:25:08 +0200

    fixes

--- .config/nvim/after/plugin/bufferline.lua
+++ .config/nvim/after/plugin/bufferline.lua
@@ -7,7 +7,10 @@ require('bufferline').setup {
                 text_align = 'left',
                 separator = true
             }
-        }
+        },
+        show_buffer_close_icons = false,
+        show_buffer_default_icon = true,
+        show_close_icon = false,
     }
 }
 
