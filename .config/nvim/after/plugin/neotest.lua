8eea9d096e6dd7c66d83f2646b2ad038022c92f6
Dmitry Kudryavtsev <dmitry.kudryavtsev@forter.com>
2023-02-16 10:11:06 +0200

    add jest test runner

--- .config/nvim/after/plugin/neotest.lua
+++ .config/nvim/after/plugin/neotest.lua
@@ -1,17 +1,20 @@
 require 'neotest'.setup {
-  adapter = {
-    require('neotest-jest')({
-      jestCommand = 'npm test --',
-      jestConfigFile = 'jest.config.js'
-    }),
-    -- require('neotest-vim-test')({
-    --   ignore_file_types = { "typescript", "javascript" }
-    -- })
-  },
-  diagnostic = {
-    enabled = true
-  },
-  status = {
-    enabled = true
-  }
+    adapter = {
+        require('neotest-jest')({
+            jestCommand = 'npm test --',
+            jestConfigFile = 'jest.config.js',
+            cwd = function(path)
+                return vim.fn.getcwd()
+            end
+        }),
+        -- require('neotest-vim-test')({
+        --   ignore_file_types = { "typescript", "javascript" }
+        -- })
+    },
+    diagnostic = {
+        enabled = true
+    },
+    status = {
+        enabled = true
+    }
 }
